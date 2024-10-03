80d2f50e810ec0753f9358f13269d87d85807566
Ritik Bhandari <bhandariritik825@gmail.com>
2024-10-03 23:00:45 +0530

    Update labq1.cpp

--- labq1.cpp
+++ labq1.cpp
@@ -9,29 +9,74 @@ If the charge exceeds Rs 300, a 15% surcharge is added.*/
 
 #include <iostream>
 using namespace std;
-int main()
+class bill
 {
+private:
  string name;
  int units;
- float bill = 0.0;
- cout << "Enter your name: ";
- getline(cin, name);
- cout << "Enter the number of units consumed: ";
- cin >> units;
- if (units <= 100) {
- bill = units * 0.60;
- } else if (units <= 300) {
- bill = (100 * 0.60) + ((units - 100) * 0.80);
- } else {
- bill = (100 * 0.60) + (200 * 0.80) + ((units - 300) * 0.90);
+ float cost;
+public:
+ void setName(string name)
+ {
+ this->name = name;
  }
- if (bill < 50) {
- bill = 50;
+ void setUnits(int units)
+ {
+ this->units = units;
  }
- if (bill > 300) {
- bill += bill * 0.15;
+ void getBill()
+ {
+ int x = this->units;
+ if (x <= 100)
+ {
+ this->cost = x * 60;
  }
- 
- cout<< name << "! Your electricity bill is Rs " << bill << endl;
- return 0;
+ else if (x <= 300)
+ {
+ this->cost = 100 * 60 + (x - 100) * 80;
+ }
+ else
+ {
+ this->cost = 100 * 60 + 200 * 80 + (x - 300) * 90;
+ }
+ this->cost /= 100;
+ if (this->cost < 50)
+ {
+ this->cost += 50;
+ }
+ if (this->cost > 300)
+ {
+ this->cost += 0.15 * this->cost;
+ }
+ }
+ void output()
+ {
+ cout << "name: " << this->name << endl;
+ cout << "no of units: " << this->units << endl;
+ cout << "total bill: Rs " << this->cost << endl;
+ }
+};
+int main()
+{
+ int n;
+ cout << "enter number of users" << endl;
+ cin >> n;
+ bill c[n];
+ for (int i = 0; i < n; i++)
+ {
+ string s;
+ cout << "enter name:" << endl;
+ cin >> s;
+ int x;
+ cout << "enter units:" << endl;
+ cin >> x;
+ c[i].setName(s);
+ c[i].setUnits(x);
+ c[i].getBill();
+ }
+ cout << "\nElectricity bill details:" << endl;
+ for (int i = 0; i < n; i++)
+ {
+ c[i].output();
+ }
 }
