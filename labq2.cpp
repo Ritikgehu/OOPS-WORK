f9dfd07a2ca661ffde3bd423c69152a085d7d502
Ritik Bhandari <bhandariritik825@gmail.com>
2024-10-03 23:01:12 +0530

    Update labq2.cpp

--- labq2.cpp
+++ labq2.cpp
@@ -1,5 +1,6 @@
 /* Q2.Construct a C++ program that removes a specific character from a given string and return the updated string.*/
 #include<iostream>
+#include<string>
 using namespace std;
 int main()
 {
@@ -7,13 +8,21 @@ char ch;
 string str;
 cout<<"enter string"<<endl;
 getline(cin,str);
+int l=str.length();
 cout<<"enter character to remove"<<endl;
 cin>>ch;
-for(int i=0;str[i]!='\0';i++)
+int i,j;
+for(i=0;str[i]!='\0';i++)
 {
- if(str[i]!=ch)
- cout<<str[i];
+if(str[i]==ch){
+ for(j=i;j<l;j++){
+ str[j]=str[j+1];
+ }
+ l--;
 }
- return 0;
+}
+str.resize(l);
+cout<<str;
+return 0;
 }
 
