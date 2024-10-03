3a28471f89eebe897ae68d3c2828724bb6c5ab64
Ritik Bhandari <bhandariritik825@gmail.com>
2024-10-03 23:03:50 +0530

    Update labq6.cpp

--- labq6.cpp
+++ labq6.cpp
@@ -1,28 +1,53 @@
-/*Q6. Implement a C++ program to demonstrate the concept of data abstraction using the concept of Class and 
-Objects.*/
-
+/*Define a class Hotel in C++ with the following specifications.
+Private members
+• Rno Data member to store room number
+• Name Data member to store customer name
+• Tariff Data member to store per day charges
+• NOD Data member to store number of days of stay
+• CALC() Function to calculate and return amount as NOD*Tariff ,and if the
+value of days* Tariff >10000,
+then total amount is 1.05* days*Tariff.
+Public members
+• Checkin() Function to enter the content Rno, Name, Tariff and NOD
+• Checkout() Function to display Rno, Name, Tariff,
+NOD and Amount (amount to be displayed by calling function) CALC()*/
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
-class student{
- int roll;
- string name;
-public:
- void input(){
- cout<<"Enter name of student: ";
- getline(cin,name);
- cout<<"Enter roll no. of student: ";
- cin>>roll;
- }
- void output();
-};
-void student :: output(){
- cout<<"Name of the student: "<<name;
- cout<<"\nRoll no. of student: "<<roll<<endl;
+class Hotel {
+private:
+int Rno;
+string Name;
+float Tariff;
+int nod;
+float calc() const {
+float amount = nod * Tariff;
+if (amount > 10000) {
+return 1.05 * amount;
+} else {
+return amount;
 }
+}
+public:
+void Checkin() {
+cout << "Enter room number=";
+cin >> Rno;
+cin.ignore();
+cout << "Enter customer name=";
+getline(cin, Name);
+cout << "Enter tariff per day=";
+cin >> Tariff;
+cout << "Enter number of days of stay=";
+cin >> nod; }
+void Checkout() const {
+cout << "Room Number=" << Rno << endl;
+cout << "Customer Name=" << Name << endl;
+cout << "Tariff per day=" << Tariff << endl;
+cout << "Number of Days of Stay=" << nod << endl;
+cout << "Amount=" << calc() << endl; }};
 int main() {
- student s1;
- s1.input();
- s1.output();
+Hotel hotel;
+hotel.Checkin();
+hotel.Checkout();
 return 0;
 }
