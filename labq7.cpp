432b6a779c72c892077d5ea8d5172956839637eb
Ritik Bhandari <bhandariritik825@gmail.com>
2024-10-03 23:04:20 +0530

    Update labq7.cpp

--- labq7.cpp
+++ labq7.cpp
@@ -1,43 +1,67 @@
-/*Q7. Write a C++ program to store and display the record of N Students.*/
-
+/*Implement a Program in C++ by defining a class to represent a bank account. Include the following:
+Data Members
+• Name of the depositor
+• Account number
+• Type of account (Saving, Current etc.)
+• Balance amount in the account
+Member Functions
+• To assign initial values
+• To deposit an amount
+• To withdraw an amount after checking the balance
+To display name and balance*/
 #include <iostream>
 #include <string>
 using namespace std;
-class Student
-{
+class BankAccount {
 private:
- string name;
- int marks;
+string depositorName;
+string accountNumber;
+string accountType;
+float balance;
 public:
- void getDetails();
- void setDetails();
+BankAccount(string name, string accNum, string accType, float initialBalance) {
+depositorName = name;
+accountNumber = accNum;
+accountType = accType;
+balance = initialBalance;
+}
+void deposit(float amount) {
+if (amount > 0) {
+balance += amount;
+} else {
+cout << "Deposit amount must be positive!" << endl;
+}
+}
+void withdraw(float amount) {
+if (amount > 0) {
+if (amount <= balance) {
+balance -= amount;
+} else {
+cout << "Insufficient balance!" << endl;
+}
+} else {
+cout << "Withdrawal amount must be positive!" << endl;
+}
+}
+void displayAccountInfo() const {
+cout << "Account Holder=" << depositorName << endl;
+cout << "Account Number=" << accountNumber << endl;
+cout << "Account Type=" << accountType << endl;
+cout << "Current Balance=" << balance << endl;
+}
 };
-void Student::setDetails()
-{
- cout << "Enter the name:" << endl;
- cin >> name;
- cout << "Enter total marks:" << endl;
- cin >> marks;
-}
-void Student::getDetails()
-{
- cout << "Name: " << name << " ,marks: " << marks << endl;
-}
-int main()
-{
- int count;
- cout << "Enter the total number of students: ";
- cin >> count;
- Student studentArray[count];
- for (int i = 0; i < count; i++)
- {
- cout << "For student " << i + 1 << ":" << endl;
- studentArray[i].setDetails();
- }
- cout << "\nStudent details " << endl;
- for (int i = 0; i < count; i++)
- {
- studentArray[i].getDetails();
- }
- return 0;
+int main() {
+float n,m;
+int z;
+BankAccount myAccount("RITIK BHANDARI", "230112016", "Savings", 59000.00);
+myAccount.displayAccountInfo();
+cout<<"\nEnter the amount to be deposited="<<endl;
+cin>>m;
+myAccount.deposit(m);
+cout<<"\nEnter the amount to be withdrawn="<<endl;
+cin>>n;
+myAccount.withdraw(n);
+cout<<"\nAccount Details="<<endl;
+myAccount.displayAccountInfo();
+return 0;
 }
